5bedfc61e49788b84b67ed67f5435a851d00a3d9
Danil Deineka <tripledpadik@gmail.com>
2024-05-15 14:48:54 +0300

    Make weather app

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,123 @@
-<template>
-  <input type="text" v-model="userName" placeholder="Имя" />
-  <input type="password" v-model="userPass" placeholder="Пароль" />
-  <input type="email" v-model="userEmail" placeholder="Email" />
-  <button @click="sendData">отправить</button>
-  <p className="error">{{ error }}</p>
-
-  <div v-if="users.length === 0" className="user">У нас нет пользователей</div>
-
-  <div v-else className="user">У нас {{ users.length }} пользователей</div>
-
-  <User
-    v-for="(el, index) in users"
-    :key="index"
-    :user="el"
-    :index="index"
-    :deleteUser="deleteUser"
-  />
-</template>
-
 <script>
-import User from "./components/User.vue";
+import axios from "axios";
 
 export default {
-  components: { User },
   data() {
     return {
-      users: [],
+      city: "",
       error: "",
-      userName: "",
-      userPass: "",
-      userEmail: "",
+      info: null,
     };
   },
-  methods: {
-    sendData() {
-      if (this.userName === "") {
-        this.error = "Имя не введено";
-        return;
-      }
-
-      if (this.userPass === "") {
-        this.error = "Пароль не введено";
-        return;
-      }
-
-      if (this.userEmail === "") {
-        this.error = "Email не введено";
-        return;
-      }
-
-      this.error = "";
-
-      this.users.push({
-        name: this.userName,
-        password: this.userPass,
-        email: this.userEmail,
-      });
+  computed: {
+    cityName() {
+      return "«" + this.city + "»";
     },
+    showTemp() {
+      return "Температура: " + this.info.main.temp;
+    },
+    showFeelsLike() {
+      return "Ощущаеться как: " + this.info.main.feels_like;
+    },
+    showMinTemp() {
+      return "Минимальная температура: " + this.info.main.temp_min;
+    },
+    showMaxTemp() {
+      return "Максимальная температура: " + this.info.main.temp_max;
+    },
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Нужно название более одного символа";
+        return false;
+      } else {
+        this.error = "";
+      }
 
-    deleteUser(index) {
-      this.users.splice(index, 1);
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=1d3d7ec9e2cbbb5cd932b49f524ffe76`
+        )
+        .then((res) => (this.info = res.data));
     },
   },
 };
 </script>
 
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в {{ city === "" ? "вашем городе" : cityName }}</p>
+    <input type="text" v-model="city" placeholder="Введите город" />
+    <button v-if="city" @click="getWeather()">Получить погоду</button>
+    <button disabled v-else="city">Введите название города</button>
+    <p class="error">{{ error }}</p>
+
+    <div v-if="info !== null">
+      <p>{{ showTemp }}</p>
+      <p>{{ showFeelsLike }}</p>
+      <p>{{ showMinTemp }}</p>
+      <p>{{ showMaxTemp }}</p>
+    </div>
+  </div>
+</template>
+
 <style scoped>
-input {
-  display: block;
-  margin-bottom: 10px;
-  border-radius: 3px;
-  border: 1px solid silver;
-  outline: none;
-  padding: 10px 15px;
-  background-color: #fafafa;
-  color: #333;
+.error {
+  color: #d03939;
 }
 
-button {
-  border: 0;
-  border-radius: 5px;
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: #1f0f24;
+  text-align: center;
+  color: #fff;
+}
+
+.wrapper h1 {
+  margin-top: 50px;
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
   outline: none;
+}
+
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.wrapper button:disabled {
+  background-color: #746027;
+  cursor: not-allowed;
+}
+
+.wrapper button {
+  background-color: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
   padding: 10px 15px;
-  background-color: #6cd670;
-  color: #167f3d;
-  font-weight: bold;
+  margin-left: 20px;
   cursor: pointer;
   transition: transform 500ms ease;
 }
 
-button:hover {
-  transform: translateY(-5px);
-}
-
-.user {
-  width: 500px;
-  margin-top: 20px;
-  border: 1px solid silver;
-  background-color: #e3e3e3;
-  color: #222;
-  padding: 20px;
-  border-radius: 5px;
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
